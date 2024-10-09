98862e5d3941ec57c1ecd75895ccba5f1cee03cc
subashree-tech <subashree@klmsolutions.in>
2024-10-09 14:10:00 +0530

    Update cybersecurity_app.py

--- cybersecurity_app.py
+++ cybersecurity_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
-from langchain.chat_models import ChatOpenAI
-from langchain.embeddings import OpenAIEmbeddings
+from langchain_community.chat_models import ChatOpenAI
+from langchain_community.embeddings import OpenAIEmbeddings
 from langchain.chains import LLMChain
 import psycopg2
 
